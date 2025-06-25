0fe0809103196a21d7d3188b6d12600265d928e3
Giulia Stefani <giuliastefani00@gmail.com>
2025-06-25 17:27:45 -0300

    Alterações no banco

--- cantina.sql
+++ cantina.sql
@@ -1,43 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25/06/2025 às 21:10
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `cantina`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `itens_venda`
---
-
 CREATE TABLE `itens_venda` (
+  `id` int(11) NOT NULL,
   `id_venda` int(11) NOT NULL,
   `id_produto` int(11) NOT NULL,
   `quantidade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `produto`
---
 
 CREATE TABLE `produto` (
   `id` int(11) NOT NULL,
@@ -46,45 +12,12 @@ CREATE TABLE `produto` (
   `quantidade_estoque` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `produto`
---
-
-INSERT INTO `produto` (`id`, `nome`, `valor_unitario`, `quantidade_estoque`) VALUES
-(1, 'Coca-Cola Lata 350ml', 5.00, 150),
-(2, 'Salgado Assado (Esfiha de Carne)', 7.50, 40),
-(3, 'Pão de Queijo (unidade)', 4.50, 80),
-(4, 'Água Mineral sem Gás 500ml', 3.00, 200),
-(5, 'Suco de Laranja Natural 300ml', 8.00, 25),
-(6, 'Chocolate Kit Kat 41,5g', 4.00, 120),
-(7, 'Salgado Frito (Coxinha de Frango)', 7.50, 45),
-(8, 'Chiclete Trident (unidade)', 2.50, 300),
-(9, 'Café Expresso', 5.50, 100),
-(10, 'Sanduíche Natural de Frango', 12.00, 15),
-(11, 'Bolo de Chocolate (fatia)', 9.00, 10),
-(12, 'Fini Tubes Morango (pacote)', 6.00, 90),
-(13, 'Biscoito Passatempo Recheado', 5.00, 60),
-(14, 'Guaraná Antarctica Lata 350ml', 5.00, 140),
-(15, 'Paçoca (unidade)', 1.50, 250);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
-
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `login` varchar(20) NOT NULL,
   `senha` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `venda`
---
 
 CREATE TABLE `venda` (
   `id` int(11) NOT NULL,
@@ -93,84 +26,39 @@ CREATE TABLE `venda` (
   `vendedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `itens_venda`
---
 ALTER TABLE `itens_venda`
-  ADD PRIMARY KEY (`id_venda`),
-  ADD KEY `id_produto` (`id_produto`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_venda_idx` (`id_venda`),
+  ADD KEY `fk_produto_idx` (`id_produto`);
 
---
--- Índices de tabela `produto`
---
 ALTER TABLE `produto`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nome` (`nome`);
 
---
--- Índices de tabela `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `login` (`login`);
 
---
--- Índices de tabela `venda`
---
 ALTER TABLE `venda`
   ADD PRIMARY KEY (`id`),
   ADD KEY `vendedor` (`vendedor`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `itens_venda`
---
 ALTER TABLE `itens_venda`
-  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de tabela `produto`
---
 ALTER TABLE `produto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
---
--- AUTO_INCREMENT de tabela `usuario`
---
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de tabela `venda`
---
 ALTER TABLE `venda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `itens_venda`
---
 ALTER TABLE `itens_venda`
-  ADD CONSTRAINT `itens_venda_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `venda` (`id`),
-  ADD CONSTRAINT `itens_venda_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`);
+  ADD CONSTRAINT `fk_itens_venda_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`),
+  ADD CONSTRAINT `fk_itens_venda_venda` FOREIGN KEY (`id_venda`) REFERENCES `venda` (`id`);
 
---
--- Restrições para tabelas `venda`
---
 ALTER TABLE `venda`
   ADD CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`vendedor`) REFERENCES `usuario` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
